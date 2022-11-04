15edbfb1e6ac14dae1092aa8b27d40220817ad36
Breno Rodrigues <brenocartoon@hotmail.com>
2022-11-04 00:43:39 -0300

    feat: setting up svg transformer

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Center, Text } from "native-base";
+import Logo from "../assets/logo.svg";
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
+      <Logo />
       <Text color="white" fontSize="24">SignIn now! =D</Text>
     </Center>
   );
